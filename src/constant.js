d409c31ff81362d90a1ebfa46b1478471f179273
fdemir <frndemir@gmail.com>
2023-02-09 02:01:51 +0300

    refactor: set zoom lvl as constnat

--- src/constant.js
+++ src/constant.js
@@ -43,3 +43,6 @@ export const CITIES = [
 
 export const DEFAULT_LOC = [37.58333, 36.93333];
 export const BASE_URL = "https://apigo.afetharita.com/feeds/areas";
+
+export const MIN_ZOOM = 14;
+export const MAX_ZOOM = 18;
