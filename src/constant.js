915fe32b62c435d9a0b19942941c7041747abc0e
fdemir <frndemir@gmail.com>
2023-02-09 01:56:36 +0300

    refactor

---
+++ src/constant.js
@@ -0,0 +1,45 @@
+export const CITIES = [
+  {
+    city: "ADANA",
+    coordinates: [37.00167, 35.32889],
+  },
+  {
+    city: "ADIYAMAN",
+    coordinates: [37.76472, 38.27861],
+  },
+  {
+    city: "MALATYA",
+    coordinates: [38.355, 38.30972],
+  },
+  {
+    city: "OSMANIYE",
+    coordinates: [37.07417, 36.24694],
+  },
+  {
+    city: "GAZIANTEP",
+    coordinates: [37.06667, 37.38333],
+  },
+  {
+    city: "HATAY",
+    coordinates: [36.2, 36.16667],
+  },
+  {
+    city: "ŞANLIURFA",
+    coordinates: [37.15, 38.8],
+  },
+  {
+    city: "KİLİS",
+    coordinates: [36.71667, 37.11667],
+  },
+  {
+    city: "DİYARBAKIR",
+    coordinates: [37.91417, 40.23056],
+  },
+  {
+    city: "KAHRAMANMARAŞ",
+    coordinates: [37.58333, 36.93333],
+  },
+];
+
+export const DEFAULT_LOC = [37.58333, 36.93333];
+export const BASE_URL = "https://apigo.afetharita.com/feeds/areas";
