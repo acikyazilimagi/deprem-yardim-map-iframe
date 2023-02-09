161b4a1294f4a9f7e2be009a9f7509f26daf9960
fdemir <frndemir@gmail.com>
2023-02-09 04:35:59 +0300

    optimizer

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,7 @@
-import { defineConfig } from 'astro/config';
+import compress from "astro-compress";
+import { defineConfig } from "astro/config";
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  integrations: [compress()],
+});
